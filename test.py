9e043152ffae6f6ef597a462263bfb5591db968e
Akeem King <akeemtlking@gmail.com>
2023-02-04 08:27:23 -0500

    Updating test to reflect changes

--- test.py
+++ test.py
@@ -1,13 +1,18 @@
 import requests
 
-
 """
 Reads nutrition file from example directory and attempts to convert it to json then prints the result
 """
 def run_test():
-	print("Testing with nutrition image...")
-	with open("examples/nutrition_facts.jpeg", 'rb') as f:
+	file_name = "examples/nutrition_facts.jpeg" 
+	print(f"testing nutrition scan w/ {file_name}")
+	with open(file_name, 'rb') as f:
 		response = requests.post('http://127.0.0.1:5000/scan?user=automated&type=nutrition', data=f)
 		print(response.json())
+
+	print(f"testing generic scan w/ {file_name}")
+	with open(file_name, 'rb') as f:
+		response = requests.post('http://127.0.0.1:5000/scan?user=automated', data=f)
+		print(response.text)
 
 run_test()
